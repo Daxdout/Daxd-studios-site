70c9597b395dcfc733c5a9334a92b3186fb824c7
Daksh <dhirandaksh@gmail.com>
2026-06-27 12:45:31 +0530

    changes testemonial

--- daxd-studios/src/components/Testimonials.jsx
+++ daxd-studios/src/components/Testimonials.jsx
@@ -1,18 +1,4 @@
-import testimonial1 from '../assets/testimonial1.jpg'
-import testimonial2 from '../assets/testimonial2.jpg'
-import testimonial3 from '../assets/testimonial3.jpg'
-import testimonial4 from '../assets/testimonial4.jpg'
-import testimonial5 from '../assets/testimonial5.jpg'
-import testimonial6 from '../assets/testimonial6.jpg'
-
-const testimonials = [
-  testimonial1,
-  testimonial2,
-  testimonial3,
-  testimonial4,
-  testimonial5,
-  testimonial6,
-]
+import testimonialsImage from '../assets/testimonials-collage.png'
 
 export default function Testimonials() {
   return (
@@ -32,7 +18,7 @@ export default function Testimonials() {
         <div
           style={{
             textAlign: 'center',
-            marginBottom: '120px',
+            marginBottom: '80px',
           }}
         >
           <div
@@ -71,38 +57,31 @@ export default function Testimonials() {
           </p>
         </div>
 
-        {/* Testimonials */}
+        {/* Single Image */}
         <div
           style={{
-            display: 'grid',
-            gridTemplateColumns: 'repeat(2, auto)',
+            display: 'flex',
             justifyContent: 'center',
-            columnGap: '140px',
-            rowGap: '120px',
-            marginBottom: '120px',
+            overflow: 'hidden',
           }}
         >
-          {testimonials.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt={`Testimonial ${index + 1}`}
-              style={{
-                width: '220px',
-                height: 'auto',
-                transform: 'scale(3)',
-                transformOrigin: 'center',
-                transition: 'transform 0.3s ease',
-                cursor: 'pointer',
-              }}
-              onMouseEnter={(e) => {
-                e.currentTarget.style.transform = 'scale(3.5)'
-              }}
-              onMouseLeave={(e) => {
-                e.currentTarget.style.transform = 'scale(3)'
-              }}
-            />
-          ))}
+          <img
+            src={testimonialsImage}
+            alt="Client Testimonials"
+            style={{
+              width: '100%',
+              maxWidth: '1450px',
+              transform: 'scale(1.15)',
+              transformOrigin: 'center',
+              transition: 'transform 0.3s ease',
+            }}
+            onMouseEnter={(e) => {
+              e.currentTarget.style.transform = 'scale(1.2)'
+            }}
+            onMouseLeave={(e) => {
+              e.currentTarget.style.transform = 'scale(1.15)'
+            }}
+          />
         </div>
       </div>
     </section>
