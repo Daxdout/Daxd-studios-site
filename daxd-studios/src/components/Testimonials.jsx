52adc4bac458f4e930e0bba9638bf3b8e91535d5
Daksh <dhirandaksh@gmail.com>
2026-06-30 09:49:49 +0530

    changfs

--- daxd-studios/src/components/Testimonials.jsx
+++ daxd-studios/src/components/Testimonials.jsx
@@ -69,7 +69,7 @@ export default function Testimonials() {
             src={testimonialsImage}
             alt="Client Testimonials"
             style={{
-              width: '65%',
+              width: '70%',
               maxWidth: '1450px',
               transform: 'scale(1.15)',
               transformOrigin: 'center',
